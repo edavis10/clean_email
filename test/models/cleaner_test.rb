48fd57b5b695fba64a4391256ca44c0aeda7412f
Eric Davis <edavis@littlestreamsoftware.com>
2014-06-24 10:52:34 -0700

    Spec out Cleaner#plain_text

--- test/models/cleaner_test.rb
+++ test/models/cleaner_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CleanerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#plain_text should return the user's content as is" do
+    cleaner = Cleaner.new(:content => "TestA")
+
+    assert_equal cleaner.content, cleaner.plain_text
+  end
+
+  test "#plain_text should wrap lines at 67 columns"
+
 end
